b32a0f92a265605160ef82fc27b03fee0daf2dd7
Nguyenvaan77 <anvan16022005@gmail.com>
2025-09-14 17:38:27 +0700

    init project

---
+++ src/main/java/com/basis/anhangda37/LaptopShopApplication.java
@@ -0,0 +1,12 @@
+package com.basis.anhangda37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopShopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/basis/anhangda37/Anhangda37ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basis.anhangda37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anhangda37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
