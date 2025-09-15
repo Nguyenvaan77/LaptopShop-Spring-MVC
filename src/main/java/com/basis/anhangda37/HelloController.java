bd5e5d55a511876a7b2d95b043e35b3a2183a5fb
Nguyenvaan77 <anvan16022005@gmail.com>
2025-09-15 20:02:44 +0700

    abc

--- src/main/java/com/basis/anhangda37/HelloController.java
+++ src/main/java/com/basis/anhangda37/HelloController.java
@@ -3,12 +3,21 @@ package com.basis.anhangda37;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello world from Spring boot";
+        return "Hello world from Spring ";
+    }
+
+    @GetMapping("/user")
+    public String indexUser() {
+        return "Only User con access this api";
+    }   
+    
+    @GetMapping("/admin")
+    public String indexAdmin() {
+        return "Only Admin con access this api";
     }
 }
